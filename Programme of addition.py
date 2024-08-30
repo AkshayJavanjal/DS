58a038dc1e26a52189bb74de269a5feeef16d4b1
Akshay Atul Javanjal <akshayjavanjal1996@gmail.com>
2024-08-30 11:43:02 +0530

    2st

---
+++ Programme of addition.py
@@ -0,0 +1,24 @@
+name= "Tony Stark"
+print(name.upper())
+print(name.lower())
+print(name.find('S'))
+print(name.find('r'))
+print(name.replace("Stark","IronMan"))
+print('T' in name)
+print('m' in name)
+print('Stark' in name)
+print(2+5)
+print(5-3)
+print(5*2)
+print(11/2)
+print(11//2)
+print(5%2)
+print(11%2)
+print(5**2)
+print(10**2)
+print(3**3)
+i=1
+i=i+2
+i+=2
+i*=2
+i-=2
